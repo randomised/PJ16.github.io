861f489e4dbb4d9f3436241b4a46f54f3e1e9fbf
randomised <53529958+randomised@users.noreply.github.com>
2021-04-14 16:05:56 +0100

    Add files via upload

---
+++ console.js
@@ -0,0 +1,15 @@
+console.log("Welcome! : To my website")
+console.log("Project Links : A few projects im working on!")
+console.log("https://randomised.github.io/Windows%20Flag.html")
+console.log("Currently Unfinished Website (insert image gag here)")
+
+
+
+
+console.log("")
+console.log("")
+console.log("")
+console.log("")
+console.log("")
+console.log("")
+console.log("Loading Complete")
